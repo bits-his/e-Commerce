01c2a9c8538e79964409a841a1c0d079e745050c
69rc <jibrinb271@gmail.com>
2024-07-18 16:41:35 +0100

    save

---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FaSearch } from 'react-icons/fa'; // Assuming you are using react-icons for icons
+
+function Header() {
+  return (
+    <div className="bg-dark header">
+      <div className="">
+       hi
+      </div>
+    </div>
+  );
+}
+
+export default Header;
