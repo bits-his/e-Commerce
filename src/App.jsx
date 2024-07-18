749faed0982ac3da9489118d44ddb80b6a661ba9
Phisherman <117809934+Phisherman-0@users.noreply.github.com>
2024-07-18 12:35:51 +0100

    save

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import {Layout} from './components/Layout'
+
+
+const App = ()=>{
+  return(
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout/>}>
+            <Route path='/' element={<Home />}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App;
