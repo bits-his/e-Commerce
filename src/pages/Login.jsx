275a0f2baf9b7bfbba5e263e41b153417d179368
unknown <aishakabir964@gmail.com>
2024-07-18 06:03:16 -0700

    save

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import { Form } from 'react-bootstrap'
+import { Button, Form } from 'react-bootstrap'
+import '../Styles/Login.css'
 
 function Login() {
   return (
     <>
-        <div>
+        <div className='login'>
+        <h2>Login</h2>
             <Form>
                 <Form.Label htmlFor="user">User ID</Form.Label>
                 <Form.Control
@@ -18,6 +20,14 @@ function Login() {
                     id="pass"
                     name='password'
                 />
+                <div className='btn-container'>
+                <Button variant='primary' type='submit' className='btn-primary'>
+                    Login
+                </Button>
+                </div>
+                <div className='d-flex' style={{justifyContent:"center"}}>
+                    <h6>Dont have an account? <span>Register Now</span> </h6>
+                </div>
             </Form>
         </div>
     </>
