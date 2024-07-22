8e42b1b911c79d46a6d6d9a7691f289c4fac4a86
abbaboss2023 <abbaboss2023@gmail.com>
2024-07-22 12:12:46 +0100

    save

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -9,11 +9,11 @@ const Sidebar = () => {
   return (
     <div className="side-nav bg-dark">
       <Nav className="d-flex flex-column h-full w-100 px-3">
-        <div className="d-flex justify-content-between align-items-center">
-          <span></span>
-          <span></span>
-        </div>
         <div className="d-flex flex-column nav-container">
+          <div className="d-flex justify-content-center">
+            <div className="brand-logo"></div>
+          </div>
+          
           <DropdownBtn
             title="Dashboard"
             items={["Overview", "Sales Analytics", "Recent Orders", "Revenue Reports", "Traffic Insights"]}
@@ -31,16 +31,26 @@ const Sidebar = () => {
             items={["Admin Users", "Roles & Permissions", "Activity Logs"]}
           />
 
-          <DropdownBtn  
-            title="User Management"
-            items={["Admin Users", "Roles & Permissions", "Activity Logs"]}
+          {/* Link to sellers route */}
+
+          <DropdownBtn
+            title="Store Management"
+            items={["Store profile", "Payment method"]}
           />
+
+          <DropdownBtn
+            title="Product Management"
+            items={["Product", "Payment method"]}
+          />
+
         </div>
       </Nav>
-      <div className="logout">
-        <NavLink>
-          Logout
-        </NavLink>
+      <div className="logout-main">
+        <div className="logout-container">
+          <NavLink className="logout">
+            Logout
+          </NavLink>
+        </div>
       </div>
     </div>
   );
