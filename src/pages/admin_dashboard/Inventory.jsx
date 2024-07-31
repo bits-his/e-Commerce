0649072683725a9e8dad7585945eebdf092bc2e1
Phisherman <117809934+Phisherman-0@users.noreply.github.com>
2024-07-31 16:41:27 +0100

    save

--- src/pages/admin_dashboard/Inventory.jsx
+++ src/pages/admin_dashboard/Inventory.jsx
@@ -12,19 +12,29 @@ const InventoryItem = () => {
   return (
     <Col lg={4} md={12} className="d-flex px-md-2 px-sm-3 py-2">
       <div className="card w-100 p-2">
-        <div className="d-flex align-items-start justify-content-between mb-3">
+        <div className="d-flex align-items-start justify-content-between mb-3 mt-0">
           <div className="d-flex align-items-center">
-            <div className="p-4 rounded bg-dark"></div>
+            <div className="rounded bg-dark" style={{padding: '2.4rem'}}></div>
             <div className="d-flex flex-column ms-2">
-              <span>Seller name</span>
-              <small>#124.43</small>
+                <div className="d-flex flex-column mb-2">
+                    <span style={{fontSize: '1rem', fontWeight: '600', overflowX: 'auto'}}>Seller name</span>
+                    <i style={{fontSize: '0.8rem', overflowX: 'auto'}}>Seller name</i>
+                </div>
+              
+                <small className="fw-bold">#124.43</small>
             </div>
           </div>
           <div className="p-1 btn btn-outline-secondary rounded d-flex align-item-center">
             <BsThreeDots />
           </div>
         </div>
-        
+        <div className="d-flex flex-column mb-3 mt-0">
+            <small className="mb-0" style={{fontSize: '1rem', fontWeight: '600'}}>wdefrgt</small>
+            <small>ynh 99 9eho v;;fdkvn; kkh i hd ioh  opjo o   oj oid </small>
+        </div>
+        <div className="d-flex flex-column mb-3 mt-0 border border-1 rounded p-2">
+            
+        </div>
       </div>
     </Col>
   );
