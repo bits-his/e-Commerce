196cac4f4003d6f57887e1964b2089c524208379
Phisherman <infernospyder@gmail.com>
2024-09-03 09:45:38 +0100

    save

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -37,8 +37,15 @@ const Sidebar = () => {
         className="flex h-full max-h-screen flex-col gap-2"
       >
         <div className="flex h-14 items-center px-4 lg:h-[60px] lg:px-6">
-          <Link className="flex items-center gap-2 text-white font-semibold">
-            <Package2 className="h-6 w-6" />
+          <Link
+            className="flex items-center gap-2 text-white justify-center font-semibold"
+            to={
+              pathname.startsWith("/admin-dashboard")
+                ? "/admin-dashboard"
+                : "/seller-dashboard"
+            }
+          >
+            <Package2 className="h-8 w-8" />
             <span className="">Kasua</span>
           </Link>
         </div>
