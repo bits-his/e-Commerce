758fd10b053602494f9e9f4da1dbccaa5047d4be
69rc <jibrinb271@gmail.com>
2024-09-09 13:07:09 +0100

    save

--- src/pages/admin_dashboard/vendors/Pending_vendors.jsx
+++ src/pages/admin_dashboard/vendors/Pending_vendors.jsx
@@ -22,9 +22,10 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { _get } from "@/utils/Helper";
 import { customerstatus } from "@/utils/Cusromer";
-import { toast } from "react-toastify";
+// import { toast } from "react-toastify";
 import { _post } from "@/utils/Helper";
 import { _delete } from "@/utils/Helper";
+import toast from "react-hot-toast";
 
 export default function Pending_customer({ args, id }) {
     const [searchQuery, setSearchQuery] = useState("");
@@ -64,7 +65,7 @@ export default function Pending_customer({ args, id }) {
             obj,
             (res) => {
                 if (res.success) {
-                    toast.success("venue updated successfully");
+                    toast.success("vendor updated successfully");
                     get_customers();
                 } else { 
                     toast.error("Error updating venue status");
